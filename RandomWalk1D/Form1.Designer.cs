20102c31c0df589661bd9783c643f50e96543051
Ibtasam Ahmad <85065814+Ibtasam-Ahmad@users.noreply.github.com>
2023-10-12 18:22:40 +0500

    Add files via upload

---
+++ RandomWalk1D/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace RandomWalk1D
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1 D Walk";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(454, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "No of Steps";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(593, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "No of Walkers";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(705, 644);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ RandomWalk1D/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomWalk1D
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+
+            int nsteps = int.Parse(textBox1.Text);
+            int nwalks = int.Parse(textBox2.Text);
+
+            //Declearing 1D Array
+            float[] x2ave = new float[nsteps];
+            float x, delx  = 3;  // x = dispalcement , delx = step size
+            Random obj = new Random();
+            double prob;
+
+            //Strating the Walk
+            for (int w = 0; w < nwalks; w++)
+            {
+                x = 0;
+                for (int s = 0; s < nsteps; s++)
+                {
+                    prob = obj.NextDouble();
+                    if (prob < 0.5)
+                    {
+                        x = x + delx;
+                    }
+                    else
+                    {
+                        x = x - delx;
+                    }
+                    x2ave[s] =x2ave[s] +  x * x;
+                } // Ending of Single Walk
+            } // Ending of All Walks
+
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Red);
+            SolidBrush sb1 = new SolidBrush(Color.Blue);
+
+            // Computing the Mean Sqaure x
+            for (int s = 0; s < nsteps; s++)
+            {
+                x2ave[s] = x2ave[s] / nwalks;
+                // By this we will get averag displacement of every walkers
+
+                gg.FillEllipse(sb, 100 + s * 10, 400 - x2ave[s], 4, 4);
+                gg.FillEllipse(sb1, 100 + s * 10, 400 - (float)Math.Sqrt(x2ave[s]), 4, 4);
+
+            }
+        }
+    }
+}
